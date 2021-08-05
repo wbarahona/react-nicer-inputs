d9bb1305a27cc2daa372a7d7fa57624cd5bde8db
Willmer Barahona <wbarahona@live.com>
2021-08-05 09:40:44 -0600

    fix on number validation
    now it only accepts numbers, for real xD

--- src/components/form/validationFactory.ts
+++ src/components/form/validationFactory.ts
@@ -16,6 +16,7 @@ export interface AssertionsProps {
 
 const emailRegex =
   /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+const numberRegex = /^[0-9]*$/;
 
 const isEmptyString = (v: string) => typeof v === 'string' && v !== '';
 
@@ -71,10 +72,8 @@ export const Assertions: AssertionsProps = {
   },
   number: (value: AnyInputType): boolean => {
     const valueStr = value as string;
-    return (
-      typeof parseInt(valueStr, 10) === 'number' ||
-      typeof parseFloat(valueStr) === 'number'
-    );
+
+    return numberRegex.test(valueStr);
   },
   pattern: (value: AnyInputType, limit): boolean => {
     const valueStr = value as string;
